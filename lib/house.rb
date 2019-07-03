2090dd581301e42299a508e5623847fec8a7a41f
Evette Telyas <etelyas@gmail.com>
2019-07-03 10:45:47 -0600

    add everything

---
+++ lib/house.rb
@@ -0,0 +1,43 @@
+class House
+  attr_reader :price, :address, :rooms, :rooms_sorted_by_category
+
+  def initialize(price, address, rooms = [], rooms_sorted_by_category = {})
+    @price = price
+    @address = address
+    @rooms = rooms
+    @rooms_sorted_by_category = rooms_sorted_by_category
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+  def rooms_from_category(category)
+    @rooms.select {|room| room.category == category}
+  end
+
+  def area
+    @rooms.map {|room| room.area}.sum
+  end
+
+  def price_float
+    (@price.delete"$").to_f
+  end
+
+  def price_per_square_foot(house)
+    (house.price_float / house.area).round(2)
+  end
+
+  def rooms_sorted_by_area
+    @rooms.sort_by {|room| room.area}.reverse
+  end
+
+
+  def rooms_sorted_by_category
+    @rooms.map do |room|
+      @rooms_sorted_by_category[room.category] = room
+    end
+    @rooms_sorted_by_category
+  end
+
+end
--- test/room_test.rb
+++ test/room_test.rb
@@ -4,4 +4,22 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
 
+  def test_room_exists
+    room = Room.new(:bedroom, 10, 13)
+
+    assert_instance_of Room, room
+  end
+
+  def test_category_exist
+    room = Room.new(:bedroom, 10, 13)
+
+    assert_equal :bedroom, room.category
+  end
+
+  def test_area
+    room = Room.new(:bedroom, 10, 13)
+
+    assert_equal 130, room.area
+  end
+
 end
